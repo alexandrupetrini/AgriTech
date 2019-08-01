fbff9f27891e34af66c682908becc88f242905a9
Alex Petrin <cyberalex4life@gmail.com>
2019-08-01 11:45:24 +0300

    Trying to get table data from mysql

--- src/components/charts/todayCharts/TodayChartsApp.js
+++ src/components/charts/todayCharts/TodayChartsApp.js
@@ -11,51 +11,47 @@ import {
 } from "recharts";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { CANCELLED } from "dns";
+// import ttn from "ttn";
+// import mysql from "mysql";
+// import moment from "moment";
+// import config from "./config";
 class TodayChartsApp extends PureComponent {
+    get_table () {
+        // const ttn = require("ttn");
+        const mysql = require("mysql");
+        const moment = require("moment");
+        const config = require("./config");
+        
+        const con = mysql.createConnection(config.databaseOptions);
+        let data = [];
+        con.connect(function (err) {
+            if (err) throw err;
+            const sql = "SELECT time, temperature, humidity FROM sensors_data";
+            con.query(sql, function (err, rows) {
+                if (err) throw err;
+                console.log("Data retrieved from table 'sensor_data'");
+                rows.forEach(row => {
+                    // let time = moment(row.time).format("hh:mma");
+                    let hours = moment(row.time).format("hh");
+                    let minutes = moment(row.time).format("mm");
+                    let time_only_in_hours = parseInt(hours, 10) + parseInt(minutes, 10) / 60;
+                    time_only_in_hours += "PM";
+                    let humidity = parseInt(row.humidity, 10) / 100;
+                    data.push({
+                        name: time_only_in_hours,
+                        Temperatura: row.temperature,
+                        Umiditate: humidity,
+                    });
+                });
+            });
+        });
+        console.log(data);
+        return data;
+    }
+
     state = {
-        data: [
-            {
-                name: "1PM",
-                Temperatura: 10
-            },
-            {
-                name: "2PM",
-                Temperatura: 13
-            },
-            {
-                name: "3PM",
-                Temperatura: 20
-            },
-            {
-                name: "4PM",
-                Temperatura: 19
-            },
-            {
-                name: "5PM",
-                Temperatura: 35
-            },
-            {
-                name: "6PM",
-                Temperatura: 32
-            },
-            {
-                name: "7PM",
-                Temperatura: 2
-            },
-            {
-                name: "8PM",
-                Temperatura: 6
-            },
-            {
-                name: "9PM",
-                Temperatura: 4
-            },
-            {
-                name: "10PM",
-                Temperatura: 10
-            }
-        ]
-    };
+        data: this.get_table()
+
     render() {
         return (
             <React.Fragment>
